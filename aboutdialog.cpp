e33b8115fc415f967c042e1cf4f3a8ffc9b2f129
figec <raptor.ext@gmail.com>
2014-08-15 20:01:15 +0400

    About dialog implemented, bump to 0.2 version :)

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,11 +1,19 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
+#include "settings.h"
+
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+
+    ui->info->setPlainText("ttyhlauncher " + Settings::launcherVerion + "\n\n" +
+                           "Это ПО с открытым исходным кодом, распространяемое под лицензией " +
+                           "GNU General Public License, версия 3.\n\n" +
+                           "https://github.com/figec/ttyhlauncher");
+    connect(ui->okButton, SIGNAL(clicked()), this, SLOT(close()));
 }
 
 AboutDialog::~AboutDialog()
