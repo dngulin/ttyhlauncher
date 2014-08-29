11041b0a2ecfdd2f87f2fc313b36190d16d43afc
figec <raptor.ext@gmail.com>
2014-08-29 14:09:43 +0400

    add splash screen while loading clients

--- main.cpp
+++ main.cpp
@@ -4,14 +4,26 @@
 #include "settings.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QBitmap>
 #include <QtCore>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Logger::logger();
+
+    QPixmap logo(":/resources/logo.png");
+    QSplashScreen* splash = new QSplashScreen(logo, Qt::FramelessWindowHint| Qt::SplashScreen);
+    splash->setMask(logo.mask());
+    splash->show();
+
     Settings::instance()->loadClientList();
-    LauncherWindow w;
+
+    splash->close();
+    delete splash;
+
+    LauncherWindow w;    
     w.show();
 
     return a.exec();
