f2c1e752a795a37a9c576e7cd2ab45628230e531
Danil Gulin <dngulin@yandex.ru>
2019-07-26 22:47:04 +0300

    Add the `PrefixVersionsIndex` class

---
+++ src/json/prefixversionsindex.cpp
@@ -0,0 +1,14 @@
+#include <QtCore/QJsonArray>
+#include "prefixversionsindex.h"
+
+Ttyh::Json::PrefixVersionsIndex::PrefixVersionsIndex(const QJsonObject &jObject)
+{
+    latest = jObject["latest"]["release"].toString();
+
+    auto jVersions = jObject["versions"].toArray();
+    foreach(auto jVersion, jVersions) {
+        auto jId = jVersion["id"];
+        if (jId.isString())
+            versions << jId.toString();
+    }
+}
