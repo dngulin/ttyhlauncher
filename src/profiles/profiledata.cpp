ad662d154a1f1b6a7c07a89b9080916d09c69f52
Danil Gulin <dngulin@yandex.ru>
2019-08-22 23:37:45 +0300

    Add window size settings into the profile data

--- src/profiles/profiledata.cpp
+++ src/profiles/profiledata.cpp
@@ -7,7 +7,11 @@ Ttyh::Profiles::ProfileData::ProfileData(const QJsonObject &jObject)
       customJavaPath(jObject[keyCustomJavaPath].toString("")),
 
       useCustomJavaArgs(jObject[keyUseCustomJavaArgs].toBool(false)),
-      customJavaArgs(jObject[keyCustomJavaArgs].toString(""))
+      customJavaArgs(jObject[keyCustomJavaArgs].toString("")),
+
+      setWindowSizeOnRun(jObject[keySetWindowSize].toBool(false)),
+      windowSizeMode(stringToSizeMode(jObject[keyWindowSizeMode].toString(modeFullScreen))),
+      windowSize(jObject[keyWindowWidth].toInt(800), jObject[keyWindowHeight].toInt(600))
 {
 }
 
@@ -23,5 +27,37 @@ QJsonObject Ttyh::Profiles::ProfileData::toJsonObject() const
     jObject.insert(keyUseCustomJavaArgs, useCustomJavaArgs);
     jObject.insert(keyCustomJavaArgs, customJavaArgs);
 
+    jObject.insert(keySetWindowSize, setWindowSizeOnRun);
+    jObject.insert(keyWindowSizeMode, sizeModeToString(windowSizeMode));
+
+    jObject.insert(keyWindowWidth, windowSize.width());
+    jObject.insert(keyWindowHeight, windowSize.height());
+
     return jObject;
+}
+
+Ttyh::Profiles::WindowSizeMode
+Ttyh::Profiles::ProfileData::stringToSizeMode(const QString &stringMode)
+{
+    if (stringMode == modeLauncherLike)
+        return WindowSizeMode::LauncherLike;
+
+    if (stringMode == modeSpecifiedSize)
+        return WindowSizeMode::Specified;
+
+    return WindowSizeMode::FullScreen;
+}
+
+QString Ttyh::Profiles::ProfileData::sizeModeToString(WindowSizeMode mode)
+{
+    switch (mode) {
+    case WindowSizeMode::LauncherLike:
+        return modeLauncherLike;
+
+    case WindowSizeMode::Specified:
+        return modeSpecifiedSize;
+
+    default:
+        return modeFullScreen;
+    }
 }
--- src/profiles/profiledata.h
+++ src/profiles/profiledata.h
@@ -2,11 +2,14 @@
 #define PROFILEDATA_H
 
 #include <QtCore/QJsonObject>
+#include <QtCore/QSize>
 
 #include "versions/fullversionid.h"
 
 namespace Ttyh {
 namespace Profiles {
+enum class WindowSizeMode { FullScreen, LauncherLike, Specified };
+
 class ProfileData
 {
 public:
@@ -21,8 +24,14 @@ public:
     bool useCustomJavaArgs;
     QString customJavaArgs;
 
+    bool setWindowSizeOnRun;
+    WindowSizeMode windowSizeMode;
+    QSize windowSize;
 
 private:
+    static WindowSizeMode stringToSizeMode(const QString &stringMode);
+    static QString sizeModeToString(WindowSizeMode mode);
+
     static constexpr const char *keyVersion = "version";
 
     static constexpr const char *keyUseCustomJavaPath = "use_custom_java_path";
@@ -30,6 +39,15 @@ private:
 
     static constexpr const char *keyUseCustomJavaArgs = "use_custom_java_args";
     static constexpr const char *keyCustomJavaArgs = "custom_java_args";
+
+    static constexpr const char *keySetWindowSize = "set_window_size";
+    static constexpr const char *keyWindowSizeMode = "window_size_mode";
+    static constexpr const char *keyWindowWidth = "window_width";
+    static constexpr const char *keyWindowHeight = "window_height";
+
+    static constexpr const char *modeFullScreen = "full-screen";
+    static constexpr const char *modeLauncherLike = "launcher-like";
+    static constexpr const char *modeSpecifiedSize = "specified-size";
 };
 }
 }
